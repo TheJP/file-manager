9005ede61280b825e8fb6c253eef9b5b3d18c43f
TheJP <jp_55555@hotmail.com>
2023-07-18 16:51:15 +0200

    Worked on meta data file storage

--- meta/src/lib.rs
+++ meta/src/lib.rs
@@ -1,10 +1,11 @@
 use std::{
+    collections::{HashMap, HashSet},
     fs::{self, File},
     io::{self, BufReader, BufWriter, Write},
     path::{Path, PathBuf},
 };
 
-use model::{PersonCollection, RootFolderCollection};
+use model::{Folder, MetaFile, PersonCollection, RootFolderCollection, RootFolderId};
 use serde::Serialize;
 
 pub mod model;
@@ -38,11 +39,19 @@ pub struct Repository {
     data_path: PathBuf,
     persons: PersonCollection,
     root_folders: RootFolderCollection,
+
+    /// Cache of folder data. Not all folder data is loaded when setting up the repository.
+    folders: HashMap<PathBuf, Folder>,
 }
 
+// TODO: Add write-lock file to data_path.
+// TODO: Add short checksum to folder meta data and data_path
+//       to check if the used ids actually reference our data_path or an other data_path.
+//       Prevents user error from causing too many inconsistencies.
 impl Repository {
     const PERSONS_FILENAME: &str = "persons.json";
     const ROOT_FOLDERS_FILENAME: &str = "root_folders.json";
+    const FOLDER_FILENAME: &str = ".jpfolder.json";
 
     pub fn load_or_create(data_path: PathBuf) -> Result<Self> {
         fs::create_dir_all(&data_path)?;
@@ -54,12 +63,14 @@ impl Repository {
             data_path,
             persons,
             root_folders,
+            folders: HashMap::with_capacity(0),
         })
     }
 
     pub fn save(&self) -> Result<()> {
         self.save_persons()?;
         self.save_root_folders()?;
+        self.save_file_data()?;
         Ok(())
     }
 
@@ -90,6 +101,73 @@ impl Repository {
             &self.root_folders,
         )
     }
+
+    pub fn save_file_data(&self) -> Result<()> {
+        for folder in self.folders.values() {
+            write(&folder.path, Self::FOLDER_FILENAME, folder)?;
+        }
+        Ok(())
+    }
+
+    pub fn load_or_create_file(
+        &mut self,
+        root_folder_id: &RootFolderId,
+        path: impl AsRef<Path>,
+    ) -> Result<&mut MetaFile> {
+        let root_folder = self
+            .root_folders
+            .root_folder(root_folder_id)
+            .ok_or(Error::InvalidRootFolder)?;
+        let absolute_path = path.as_ref().canonicalize()?;
+        if !absolute_path.starts_with(root_folder) {
+            return Err(Error::FileNotInRootFolder);
+        }
+
+        let folder_path = absolute_path.parent().ok_or(Error::InvalidRootFolder)?;
+        let folder = Self::load_or_create_folder(&mut self.folders, folder_path, root_folder_id)?;
+
+        let file_name = absolute_path
+            .file_name()
+            .ok_or(Error::InvalidFilePath)?
+            .to_os_string();
+        Ok(folder.files.entry(file_name).or_insert_with(|| MetaFile {
+            hash: None,
+            persons: HashSet::with_capacity(0),
+            tags: HashMap::with_capacity(0),
+        }))
+    }
+
+    fn load_or_create_folder<'a>(
+        folder_cache: &'a mut HashMap<PathBuf, Folder>,
+        path: impl AsRef<Path>,
+        root_folder_id: &RootFolderId,
+    ) -> Result<&'a mut Folder> {
+        let path = path.as_ref();
+        let owned_path = path.to_path_buf();
+
+        if folder_cache.contains_key(&owned_path) {
+            return Ok(folder_cache.get_mut(&owned_path).unwrap());
+        }
+
+        let meta_path = path.join(Self::FOLDER_FILENAME);
+        if meta_path.try_exists()? {
+            let meta_file = File::open(meta_path)?;
+            let meta_reader = BufReader::new(meta_file);
+            let folder = serde_json::from_reader(meta_reader)?;
+            folder_cache.insert(owned_path.clone(), folder);
+        } else {
+            folder_cache.insert(
+                owned_path.clone(),
+                Folder {
+                    path: owned_path.clone(),
+                    root_folder: *root_folder_id,
+                    files: HashMap::new(),
+                },
+            );
+        }
+
+        Ok(folder_cache.get_mut(&owned_path).unwrap())
+    }
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -97,7 +175,19 @@ pub enum Error {
     #[error("{0}")]
     IoError(#[from] io::Error),
 
+    #[error("{0}")]
+    StripPrefixError(#[from] std::path::StripPrefixError),
+
     #[error("json parsing error: {0}")]
     SerdeError(#[from] serde_json::Error),
+
+    #[error("given root folder does not exist")]
+    InvalidRootFolder,
+
+    #[error("given file is not inside provided root folder")]
+    FileNotInRootFolder,
+
+    #[error("given path does not contain a file name")]
+    InvalidFilePath,
 }
 pub type Result<T> = std::result::Result<T, Error>;
--- meta/src/model.rs
+++ meta/src/model.rs
@@ -1,88 +1,134 @@
+use crate::Result;
+
 use std::{
     collections::{HashMap, HashSet},
+    ffi::OsString,
     path::PathBuf,
 };
 
 use serde::{Deserialize, Serialize};
 use serde_with::{serde_as, skip_serializing_none};
 
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Hash, Default, Copy, Clone)]
+pub struct PersonId(usize);
+
 #[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Person {
-    pub(crate) id: usize,
-    pub(crate) name: String,
-    pub(crate) pronouns: Option<String>,
-    pub(crate) comment: Option<String>,
-    pub(crate) tags: Option<Vec<String>>,
+    pub(crate) id: PersonId,
+    pub name: String,
+    pub pronouns: Option<String>,
+    pub comment: Option<String>,
+
+    #[serde(skip_serializing_if = "HashMap::is_empty")]
+    pub tags: HashMap<String, Vec<String>>,
 }
 
 #[serde_as]
 #[derive(Debug, Default, Serialize, Deserialize)]
 pub struct PersonCollection {
-    pub(crate) next_id: usize,
+    pub(crate) next_id: PersonId,
     #[serde_as(as = "Vec<(_, _)>")]
-    pub(crate) persons: HashMap<usize, Person>,
+    pub(crate) persons: HashMap<PersonId, Person>,
 
     /// This contains tags that have been previously used for a [Person].
     ///
     /// If a tag is in this collection it does not necessarily have to
     /// currently be set as a tag on any [Person].
-    pub(crate) used_tags: HashSet<String>,
+    pub used_tags: HashSet<String>,
 }
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Hash, Default, Copy, Clone)]
+pub struct RootFolderId(usize);
 
 #[derive(Debug, Default, Serialize, Deserialize)]
 pub struct RootFolderCollection {
-    pub(crate) next_id: usize,
-    pub(crate) root_folders: HashMap<usize, PathBuf>,
+    pub(crate) next_id: RootFolderId,
+    pub(crate) root_folders: HashMap<RootFolderId, PathBuf>,
+
+    pub file_tags: HashSet<String>,
+}
+
+#[skip_serializing_none]
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct Folder {
+    pub(crate) path: PathBuf,
+    pub(crate) root_folder: RootFolderId,
+    pub(crate) files: HashMap<OsString, MetaFile>,
+}
+
+#[skip_serializing_none]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MetaFile {
+    pub hash: Option<String>,
+
+    #[serde(skip_serializing_if = "HashSet::is_empty")]
+    pub persons: HashSet<usize>,
+
+    #[serde(skip_serializing_if = "HashMap::is_empty")]
+    pub tags: HashMap<String, Vec<String>>,
 }
 
 impl PersonCollection {
-    pub fn entries(&self) -> &HashMap<usize, Person> {
+    pub fn entries(&self) -> &HashMap<PersonId, Person> {
         &self.persons
     }
 
-    pub fn add(&mut self, mut person: Person) -> usize {
+    pub fn add(&mut self, mut person: Person) -> PersonId {
         let id = self.next_id;
         person.id = id;
         self.persons.insert(id, person);
-        self.next_id += 1;
+        self.next_id.0 += 1;
         id
     }
 
-    pub fn remove(&mut self, id: &usize) -> Option<Person> {
+    pub fn remove(&mut self, id: &PersonId) -> Option<Person> {
         self.persons.remove(id)
     }
 
-    pub fn person(&self, id: &usize) -> Option<&Person> {
+    pub fn person(&self, id: &PersonId) -> Option<&Person> {
         self.persons.get(id)
     }
 
-    pub fn person_mut(&mut self, id: &mut usize) -> Option<&mut Person> {
+    pub fn person_mut(&mut self, id: &mut PersonId) -> Option<&mut Person> {
         self.persons.get_mut(id)
     }
 }
 
+impl Person {
+    pub fn new(name: String, pronouns: Option<String>) -> Self {
+        Self {
+            id: PersonId(usize::MAX),
+            name,
+            pronouns,
+            comment: None,
+            tags: HashMap::with_capacity(0),
+        }
+    }
+}
+
 impl RootFolderCollection {
-    pub fn entries(&self) -> &HashMap<usize, PathBuf> {
+    pub fn entries(&self) -> &HashMap<RootFolderId, PathBuf> {
         &self.root_folders
     }
 
-    pub fn add(&mut self, root_folder: PathBuf) -> usize {
+    pub fn add(&mut self, root_folder: PathBuf) -> Result<RootFolderId> {
+        let root_folder = root_folder.canonicalize()?;
         let id = self.next_id;
         self.root_folders.insert(id, root_folder);
-        self.next_id += 1;
-        id
+        self.next_id.0 += 1;
+        Ok(id)
     }
 
-    pub fn remove(&mut self, id: &usize) -> Option<PathBuf> {
+    pub fn remove(&mut self, id: &RootFolderId) -> Option<PathBuf> {
         self.root_folders.remove(id)
     }
 
-    pub fn root_folder(&self, id: &usize) -> Option<&PathBuf> {
+    pub fn root_folder(&self, id: &RootFolderId) -> Option<&PathBuf> {
         self.root_folders.get(id)
     }
 
-    pub fn root_folder_mut(&mut self, id: &usize) -> Option<&mut PathBuf> {
+    pub fn root_folder_mut(&mut self, id: &RootFolderId) -> Option<&mut PathBuf> {
         self.root_folders.get_mut(id)
     }
 }
