ca4e156ebf42b713fe300b158ebb051f51324e01
TheJP <jp_55555@hotmail.com>
2023-07-18 10:50:24 +0200

    Worked on meta data lib

--- meta/src/lib.rs
+++ meta/src/lib.rs
@@ -1,28 +1,33 @@
 use std::{
-    collections::HashMap,
     fs::{self, File},
     io::{self, BufReader, BufWriter, Write},
     path::{Path, PathBuf},
 };
 
-use model::{Person, PersonCollection};
+use model::{PersonCollection, RootFolderCollection};
 use serde::Serialize;
 
 pub mod model;
 
-fn read<T>(path: impl AsRef<Path>) -> Result<T>
+fn read_or_create<T>(path: &Path, file_name: impl AsRef<Path>) -> Result<T>
 where
-    T: serde::de::DeserializeOwned,
+    T: serde::de::DeserializeOwned + Default,
 {
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-    Ok(serde_json::from_reader(reader)?)
+    let path = path.join(file_name);
+    if !path.try_exists()? {
+        Ok(Default::default())
+    } else {
+        let file = File::open(path)?;
+        let reader = BufReader::new(file);
+        Ok(serde_json::from_reader(reader)?)
+    }
 }
 
-fn write<T>(path: impl AsRef<Path>, value: &T) -> Result<()>
+fn write<T>(path: &Path, file_name: impl AsRef<Path>, value: &T) -> Result<()>
 where
     T: ?Sized + Serialize,
 {
+    let path = path.join(file_name);
     let file = File::create(path)?;
     let mut writer = BufWriter::new(file);
     serde_json::to_writer(&mut writer, value)?;
@@ -32,42 +37,58 @@ where
 pub struct Repository {
     data_path: PathBuf,
     persons: PersonCollection,
+    root_folders: RootFolderCollection,
 }
 
 impl Repository {
     const PERSONS_FILENAME: &str = "persons.json";
+    const ROOT_FOLDERS_FILENAME: &str = "root_folders.json";
 
-    pub fn create_or_load(data_path: PathBuf) -> Result<Self> {
+    pub fn load_or_create(data_path: PathBuf) -> Result<Self> {
         fs::create_dir_all(&data_path)?;
 
-        let persons_path = data_path.join(Self::PERSONS_FILENAME);
-        let persons = read(persons_path)?;
+        let persons = read_or_create(&data_path, Self::PERSONS_FILENAME)?;
+        let root_folders = read_or_create(&data_path, Self::ROOT_FOLDERS_FILENAME)?;
 
-        Ok(Self { data_path, persons })
+        Ok(Self {
+            data_path,
+            persons,
+            root_folders,
+        })
     }
 
-    pub fn persons(&self) -> &HashMap<usize, Person> {
-        &self.persons.persons
+    pub fn save(&self) -> Result<()> {
+        self.save_persons()?;
+        self.save_root_folders()?;
+        Ok(())
+    }
+
+    pub fn persons(&self) -> &PersonCollection {
+        &self.persons
+    }
+
+    pub fn persons_mut(&mut self) -> &mut PersonCollection {
+        &mut self.persons
     }
 
     pub fn save_persons(&self) -> Result<()> {
-        let persons_path = self.data_path.join(Self::PERSONS_FILENAME);
-        write(persons_path, &self.persons)
+        write(&self.data_path, Self::PERSONS_FILENAME, &self.persons)
     }
 
-    pub fn add_person(&mut self, person: Person) -> usize {
-        let id = self.persons.next_id;
-        self.persons.persons.insert(id, person);
-        self.persons.next_id += 1;
-        id
+    pub fn root_folders(&self) -> &RootFolderCollection {
+        &self.root_folders
     }
 
-    pub fn person(&self, id: &usize) -> Option<&Person> {
-        self.persons.persons.get(id)
+    pub fn root_folders_mut(&mut self) -> &mut RootFolderCollection {
+        &mut self.root_folders
     }
 
-    pub fn person_mut(&mut self, id: &mut usize) -> Option<&mut Person> {
-        self.persons.persons.get_mut(id)
+    pub fn save_root_folders(&self) -> Result<()> {
+        write(
+            &self.data_path,
+            Self::ROOT_FOLDERS_FILENAME,
+            &self.root_folders,
+        )
     }
 }
 
--- meta/src/model.rs
+++ meta/src/model.rs
@@ -1,4 +1,7 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    path::PathBuf,
+};
 
 use serde::{Deserialize, Serialize};
 use serde_with::{serde_as, skip_serializing_none};
@@ -14,7 +17,7 @@ pub struct Person {
 }
 
 #[serde_as]
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Default, Serialize, Deserialize)]
 pub struct PersonCollection {
     pub(crate) next_id: usize,
     #[serde_as(as = "Vec<(_, _)>")]
@@ -25,4 +28,61 @@ pub struct PersonCollection {
     /// If a tag is in this collection it does not necessarily have to
     /// currently be set as a tag on any [Person].
     pub(crate) used_tags: HashSet<String>,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct RootFolderCollection {
+    pub(crate) next_id: usize,
+    pub(crate) root_folders: HashMap<usize, PathBuf>,
+}
+
+impl PersonCollection {
+    pub fn entries(&self) -> &HashMap<usize, Person> {
+        &self.persons
+    }
+
+    pub fn add(&mut self, mut person: Person) -> usize {
+        let id = self.next_id;
+        person.id = id;
+        self.persons.insert(id, person);
+        self.next_id += 1;
+        id
+    }
+
+    pub fn remove(&mut self, id: &usize) -> Option<Person> {
+        self.persons.remove(id)
+    }
+
+    pub fn person(&self, id: &usize) -> Option<&Person> {
+        self.persons.get(id)
+    }
+
+    pub fn person_mut(&mut self, id: &mut usize) -> Option<&mut Person> {
+        self.persons.get_mut(id)
+    }
+}
+
+impl RootFolderCollection {
+    pub fn entries(&self) -> &HashMap<usize, PathBuf> {
+        &self.root_folders
+    }
+
+    pub fn add(&mut self, root_folder: PathBuf) -> usize {
+        let id = self.next_id;
+        self.root_folders.insert(id, root_folder);
+        self.next_id += 1;
+        id
+    }
+
+    pub fn remove(&mut self, id: &usize) -> Option<PathBuf> {
+        self.root_folders.remove(id)
+    }
+
+    pub fn root_folder(&self, id: &usize) -> Option<&PathBuf> {
+        self.root_folders.get(id)
+    }
+
+    pub fn root_folder_mut(&mut self, id: &usize) -> Option<&mut PathBuf> {
+        self.root_folders.get_mut(id)
+    }
 }
