56ac00893c410be376f005e9534cf6f1c166c436
TheJP <jp_55555@hotmail.com>
2023-07-19 22:33:24 +0200

    Worked on meta data entry gui

--- file-manager/src/gui.rs
+++ file-manager/src/gui.rs
@@ -1,5 +1,8 @@
 use crate::images::ImageCache;
 
+use approximate_string_matcher::{compare, MatchResult};
+use eframe::egui::{RichText, TextEdit, Window};
+use eframe::epaint::Color32;
 use eframe::Frame;
 use eframe::{
     egui::{vec2, CentralPanel, Context, Image, Key, Ui},
@@ -9,11 +12,32 @@ use egui_extras::{Size, StripBuilder};
 
 pub(crate) struct FileManagerApp {
     images: ImageCache,
+    meta_window_open: bool,
+    meta_search: String,
+    meta_options: Vec<MetaOption>,
+    persons: Vec<String>,
+}
+
+pub(crate) enum MetaOption {
+    Create,
+    MatchResult(MatchResult),
+}
+
+impl From<MatchResult> for MetaOption {
+    fn from(value: MatchResult) -> Self {
+        MetaOption::MatchResult(value)
+    }
 }
 
 impl FileManagerApp {
     pub(crate) fn new(images: ImageCache) -> Self {
-        Self { images }
+        Self {
+            images,
+            meta_window_open: false,
+            meta_search: String::new(),
+            persons: vec!["Janis".into(), "Jaguar Nano".into()],
+            meta_options: vec![MetaOption::Create],
+        }
     }
 
     fn add_image(&mut self, ctx: &Context, ui: &mut Ui) {
@@ -49,11 +73,79 @@ impl FileManagerApp {
             self.images.back();
         }
     }
+
+    fn meta_hotkeys(&mut self, ctx: &Context) {
+        if ctx.input(|input| input.key_pressed(Key::Num1)) {
+            self.meta_search = String::new();
+            self.meta_options = vec![MetaOption::Create];
+            self.meta_window_open = true;
+        }
+    }
+
+    fn meta_window(&mut self, ctx: &Context) {
+        Window::new("People")
+            .id(eframe::egui::Id::new("meta_window"))
+            .collapsible(false)
+            .show(ctx, |ui| {
+                let response =
+                    ui.add(TextEdit::singleline(&mut self.meta_search).hint_text("Search"));
+                response.request_focus();
+                if response.changed() {
+                    let search = self.meta_search.trim();
+                    self.meta_options = std::iter::once(MetaOption::Create)
+                        .chain(
+                            self.persons
+                                .iter()
+                                .filter_map(|name| compare(search, name))
+                                .map(Into::into),
+                        )
+                        .collect();
+                }
+
+                for options in &self.meta_options {
+                    match options {
+                        MetaOption::Create => {
+                            ui.label("Create New");
+                        }
+                        MetaOption::MatchResult(match_result) => {
+                            ui.horizontal_wrapped(|ui| {
+                                ui.spacing_mut().item_spacing.x = 0.0;
+                                let mut matches = match_result.matches();
+                                for (index, letter) in match_result.target().chars().enumerate() {
+                                    let mut letter = RichText::new(letter);
+                                    while !matches.is_empty() && matches[0] < index {
+                                        matches = &matches[1..];
+                                    }
+                                    if !matches.is_empty() && matches[0] == index {
+                                        letter = letter.color(Color32::GREEN);
+                                    }
+                                    ui.label(letter);
+                                }
+                            });
+                        }
+                    }
+                }
+            });
+    }
 }
 
 impl App for FileManagerApp {
     fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
-        self.navigate_images(ctx);
+        if !self.meta_window_open {
+            self.navigate_images(ctx);
+            self.meta_hotkeys(ctx);
+        }
+
+        if self.meta_window_open {
+            let escape = ctx.input(|input| input.key_pressed(Key::Escape));
+            let enter = ctx.input(|input| input.key_pressed(Key::Enter));
+            if escape || enter {
+                self.meta_window_open = false
+                // TODO: Handle enter
+            } else {
+                self.meta_window(ctx);
+            }
+        }
 
         CentralPanel::default().show(ctx, |ui| {
             StripBuilder::new(ui)
